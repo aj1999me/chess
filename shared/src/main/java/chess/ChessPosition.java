f5e20f9d78e409b285d721f7fbf77c087d0fc50a
aj1999me <call.aaron.jason@gmail.com>
2026-06-29 19:47:48 -0700

    passed queen tests

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,7 +1,6 @@
 package chess;
 
 import java.util.Objects;
-import java.util.ArrayList;
 
 /**
  * Represents a single square position on a chess board
@@ -34,54 +33,6 @@ public class ChessPosition {
 
     private final int col;
     private final int row;
-
-    public ChessPosition moveForward() {
-        return new ChessPosition(col, row+1);
-    }
-
-    public ChessPosition moveBack() {
-        return new ChessPosition(col, row-1);
-    }
-
-    public ChessPosition moveRight() {
-        return new ChessPosition(col+1, row);
-    }
-
-    public ChessPosition moveLeft() {
-        return new ChessPosition(col-1, row);
-    }
-
-    public ChessPosition moveForwardRight() {
-        return new ChessPosition(col+1, row+1);
-    }
-
-    public ChessPosition moveForwardLeft() {
-        return new ChessPosition(col-1, row+1);
-    }
-
-    public ChessPosition moveBackRight() {
-        return new ChessPosition(col+1, row-1);
-    }
-
-    public ChessPosition moveBackLeft() {
-        return new ChessPosition(col-1, row-1);
-    }
-
-    public boolean topEdge() {
-        return row == 8;
-    }
-
-    public boolean bottomEdge() {
-        return row == 1;
-    }
-
-    public boolean rightEdge() {
-        return col == 8;
-    }
-
-    public boolean leftEdge() {
-        return col == 1;
-    }
 
     private final static int[][] KnightMoves = {{2,1}, {1,2}, {-1,2}, {-2,1}, {-2,-1}, {-1,-2}, {1,-2}, {2,-1}};
 
