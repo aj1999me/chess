045d4817b97c9a308c88e4155565f8d306968750
aj1999me <call.aaron.jason@gmail.com>
2026-07-08 20:10:08 -0700

    completed override for deep copy of pieces in chessboard

--- shared/src/main/java/chess/ChessMap.java
+++ shared/src/main/java/chess/ChessMap.java
@@ -1,10 +1,13 @@
 package chess;
 import java.util.HashMap;
 
-public class ChessMap extends HashMap<ChessPosition, ChessMove> {
+public class ChessMap extends HashMap<ChessPosition, ChessPiece> {
     @Override
-    public ChessMap clone() throws CloneNotSupportedException {
+    public Object clone() {
         var copy = new ChessMap();
-
+        for (var key : this.keySet()) {
+            copy.put(new ChessPosition(key), new ChessPiece(this.get(key)));
+        }
+        return copy;
     }
 }
